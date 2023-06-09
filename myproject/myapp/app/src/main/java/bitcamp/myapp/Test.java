836204550e4310b82fe1c9e8841dca52a86f784e
knh0113 <vickykwak@naver.com>
2023-06-09 10:48:02 +0900

    .

--- myproject/myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myproject/myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -1,19 +1,21 @@
 package bitcamp.myapp;
 
 
+
+
 public class Test {
   
   public static void main(String[] args) {
     // 2 * 3 + 7 - 2 / 2 = ?
     // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
 
-    Calculator.init(2);
-    Calculator.multiple(3);
-    Calculator.plus(7);
-    Calculator.minus(2);
-    Calculator.divide(2);
+    bitcamp.util.Calculator.init(2);
+    bitcamp.util.Calculator.multiple(3);
+    bitcamp.util.Calculator.plus(7);
+    bitcamp.util.Calculator.minus(2);
+    bitcamp.util.Calculator.divide(2);
     
-    System.out.println(Calculator.result);
+    System.out.println(bitcamp.util.Calculator.result);
     
   }
   
