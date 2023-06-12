7544018c41b3f509329fef38f6a159a8a8495856
knh0113 <vickykwak@naver.com>
2023-06-12 14:21:24 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
@@ -1,9 +1,50 @@
 package bitcamp.myapp.vo;
 
 public class Member {
-  public int no;
-  public String name;
-  public String email;
-  public String password;
-  public char gender;
+  private int no;
+  private String name;
+  private String email;
+  private String password;
+  private char gender;
+
+  public int getNo() {
+    return this.no;
+  }
+
+  public int setNo(int no) {
+    this.no = no;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public String setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public String setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getGender() {
+    return this.gender;
+  }
+
+  public char setGender(String gender) {
+    this.gender = gender;
+  }
+
 }
