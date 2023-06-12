eedaf9f1c0cabc939a7dc2256d039cfe98006483
knh0113 <vickykwak@naver.com>
2023-06-12 16:34:21 +0900

    12번 업그레이드 계속

--- myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
@@ -1,15 +1,33 @@
 package bitcamp.myapp.vo;
 
 public class Member {
+  // 모든 신스턴스가 공유하는값은 스태틱 필드에 보관한다.
+  private static int userId = 1;
+
+  // 상수는 스태틱 필드로 정의한다.
+  // 정보를 다룰 때는 그 정보를 갖고 있는 클래스에 그 기능을 둔다.
+  // 필드도 마찬가지이다.
+  // => GRASP 패턴 : Information Expert
+  public static final char MALE = 'M';
+  public static final char FEMALE = 'W';
+
+  // 인스턴스 필드는 각각 개별적으로 유지해야 하느 값으 ㄹ저장할 때 사용한다.
+  // new 명령을 통해 변수를 heap 영역에 생성한다.
   private int no;
   private String name;
   private String email;
   private String password;
   private char gender;
 
-  public Member(int a) {
-
+  // 생성자는 인스턴스를 생성한 후 필드를 초기화시키는 일을 한다.
+  // 인스턴스를 사용할 때 문제가 없도록 유효한 값으로 초기화시킨다.
+  // 기본 생성자(default constructor)는 개발자가 생성자르 ㄹ정의하지 않을 때
+  // 컴파일러가 추가해주는 생성자다.
+  // 물론 개발자가 직접 추가할 수도 있다.
+  public Member() {
+    this.no = userId++;
   }
+
 
   public int getNo() {
     return no;
