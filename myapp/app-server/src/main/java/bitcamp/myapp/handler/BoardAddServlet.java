87da1e6579bea947e7563ab1bdca4d368294d720
knh0113 <vickykwak@naver.com>
2023-08-23 23:01:24 +0900

    jsp적용

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddServlet.java
@@ -10,9 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.NcpObjectStorageService;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 @WebServlet("/board/add")
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
@@ -22,13 +26,17 @@ public class BoardAddServlet extends HttpServlet {
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+          throws ServletException, IOException {
 
     Member loginUser = (Member) request.getSession().getAttribute("loginUser");
     if (loginUser == null) {
       response.sendRedirect("/auth/form.html");
       return;
     }
+
+    BoardDao boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
+    SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) this.getServletContext().getAttribute("sqlSessionFactory");
+    NcpObjectStorageService ncpObjectStorageService = (NcpObjectStorageService) this.getServletContext().getAttribute("ncpObjectStorageService");
 
     try {
       Board board = new Board();
@@ -39,10 +47,9 @@ public class BoardAddServlet extends HttpServlet {
 
       ArrayList<AttachedFile> attachedFiles = new ArrayList<>();
       for (Part part : request.getParts()) {
-        //        System.out.println(part.getName());
         if (part.getName().equals("files") && part.getSize() > 0) {
-          String uploadFileUrl = InitServlet.ncpObjectStorageService.uploadFile(
-              "bitcamp-nc7-bucket-20", "board/", part);
+          String uploadFileUrl = ncpObjectStorageService.uploadFile(
+                  "bitcamp-nc7-bucket-20", "board/", part);
           AttachedFile attachedFile = new AttachedFile();
           attachedFile.setFilePath(uploadFileUrl);
           attachedFiles.add(attachedFile);
@@ -50,41 +57,24 @@ public class BoardAddServlet extends HttpServlet {
       }
       board.setAttachedFiles(attachedFiles);
 
-
-      response.setContentType("text/html;charset=UTF-8");
-      PrintWriter out = response.getWriter();
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<meta charset='UTF-8'>");
-      out.printf("<meta http-equiv='refresh' content='1;url=/board/list?category=%d'>\n", board.getCategory());
-      out.println("<title>게시글</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>게시글 등록</h1>");
-      try {
-        //        System.out.println(board.getNo());
-        InitServlet.boardDao.insert(board);
-        //        System.out.println(board.getNo());
-
-        if (attachedFiles.size() > 0) {
-          int count = InitServlet.boardDao.insertFiles(board);
-          System.out.println(count);
-        }
-
-        InitServlet.sqlSessionFactory.openSession(false).commit();
-        out.println("<p>등록 성공입니다!</p>");
-
-      } catch (Exception e) {
-        InitServlet.sqlSessionFactory.openSession(false).rollback();
-        out.println("<p>등록 실패입니다!</p>");
-        e.printStackTrace();
+      boardDao.insert(board);
+      if (attachedFiles.size() > 0) {
+        boardDao.insertFiles(board);
       }
-      out.println("</body>");
-      out.println("</html>");
+
+      sqlSessionFactory.openSession(false).commit();
+      response.sendRedirect("list?category=" + request.getParameter("category"));
 
     } catch (Exception e) {
-      throw new ServletException(e);
+      sqlSessionFactory.openSession(false).rollback();
+
+      // ErrorServlet 으로 포워딩 하기 전에 ErrorServlet이 사용할 데이터를
+      // ServletRequest 보관소에 저장한다.
+      request.setAttribute("error", e);
+      request.setAttribute("message", "게시글 등록 오류!");
+      request.setAttribute("refresh", "2;url=list?category=" + request.getParameter("category"));
+
+      request.getRequestDispatcher("/error").forward(request, response);
     }
   }
 }
---
+++ myapp/app-server/src/main/webapp/index.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8'>
+<title>비트캠프</title>
+</head>
+<body>
+
+<jsp:include page="header.jsp"/>
+
+ <h1>MyApp(jsp)</h1>
+<p>실습 프로젝트 입니다</p>
+
+<jsp:include page="footer.jsp"/>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
